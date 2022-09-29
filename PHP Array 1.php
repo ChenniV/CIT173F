70a70b06578e7f3de9f010a090919a62da578789
Xavier Law <113017733+ChenniV@users.noreply.github.com>
2022-09-29 10:47:14 +0800

    Add files via upload

---
+++ PHP Array 1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$city = array("Tokyo", "Mexico City", "New York City", "Mumbai", "Seoul", "Shanghai", "Lagos", "Buenos Aires", "Cairo", "London");
+$arrlength = count($city);
+
+for($x = 0; $x < $arrlength; $x++) {
+  echo $city[$x] . " , ";
+  echo "<br>";
+}
+sort($city);
+
+array_push($city, "Los Angeles", "Calcutta", "Osaka", "Beijing");
+sort($city);
+$arrlength = count($city);
+echo "<br>";
+for($x = 0; $x < $arrlength; $x++) {
+  echo $city[$x] . " , ";
+  echo "<br>";
+}
+
+?>
+</body>
+</html>
---
+++ PHP Array 2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<title>Array Act 1</title>
+<body>
+
+<?php
+$arr = array("snow", "wind", "sunshine", "clouds", "rain", "hail", "sleet");
+echo "We've seen all kinds of weather this month. At the beginning of the month, we had " . $arr[0] . " and " . $arr[1] . ". Then came " . $arr[2] . " with a few ". $arr[3] ." and some ". $arr[4] ." . At least we didn't get any " . $arr[5] . " or " . $arr[6] .".";
+ 
+
+?>
+</body>
+</html>
